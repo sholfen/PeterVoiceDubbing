86926441181424e42cb30652c9f544bd2cdb6213
Peter Liang <sholfen@gmail.com>
2023-06-24 17:44:47 +0800

    first upload

---
+++ PeterVoiceDubbingWebAP/Data/VideoData.cs
@@ -0,0 +1,12 @@
+﻿namespace PeterVoiceDubbingWebAP.Data
+{
+    public class VideoData
+    {
+        public static string BaseUrl = string.Empty;
+
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public string VideoUrl => BaseUrl + Url;
+    }
+}
---
+++ PeterVoiceDubbingWebAP/Data/VideoService.cs
@@ -0,0 +1,26 @@
+﻿using BlazorBootstrap;
+using Microsoft.Extensions.Configuration;
+
+namespace PeterVoiceDubbingWebAP.Data
+{
+    public class VideoService
+    {
+        private readonly IConfiguration _configuration;
+
+        public VideoService()
+        {
+            string jsonPath = "videolist.json";
+            ConfigurationManager configurationManager = new ConfigurationManager();
+            var configBuilder = configurationManager.AddJsonFile(jsonPath);
+            _configuration = configBuilder.Build();
+            string baseUrl = _configuration.GetValue<string>("baseUrl");
+            VideoData.BaseUrl = baseUrl;
+        }
+
+        public List<VideoData> GetList() 
+        {
+            List<VideoData> videoDatas = _configuration.GetSection("list").Get<List<VideoData>>();
+            return videoDatas;
+        }
+    }
+}
